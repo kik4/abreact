4c2bacda4eb1c954c954b37152bcdd1ea1200f22
kik4 <kik4.contact@gmail.com>
2019-04-13 05:00:56 +0900

    add polyfill for ie11

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,7 @@
+import * as polyfill from "./polyfill";
+polyfill.set();
+require("es6-promise").polyfill();
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
