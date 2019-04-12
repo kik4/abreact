75f2c03ce860fb6b122de93b55bc34734d9fcece
kik4 <kik4.contact@gmail.com>
2019-04-13 04:15:19 +0900

    remove lazy

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,11 +6,11 @@ import routes from "../tmp/routes";
 const router = new UniversalRouter(routes);
 import HistoryContext from "./HistoryContext";
 
-class App extends React.Component<{}, { action: any }> {
+class App extends React.Component<{}, { page: any }> {
   constructor(props) {
     super(props);
     this.state = {
-      action: null
+      page: null
     };
   }
 
@@ -28,27 +28,26 @@ class App extends React.Component<{}, { action: any }> {
 
   popstate() {
     router.resolve(document.location.pathname).then(async action => {
-      this.setState({ action });
+      const module = await action();
+      this.setState({ page: module.default });
     });
   }
 
   pushstate(pathname: string) {
-    history.pushState(null, "", pathname);
-    this.popstate();
+    router.resolve(document.location.pathname).then(async action => {
+      await action();
+      history.pushState(null, "", pathname);
+      this.popstate();
+    });
   }
 
   render() {
-    const OtherComponent = React.lazy(this.state.action);
     return (
       <HistoryContext.Provider
         value={{ route: location.pathname, push: this.pushstate.bind(this) }}
       >
         <div className="App">
-          {this.state.action && (
-            <React.Suspense fallback={<div>Loading...</div>}>
-              <OtherComponent />
-            </React.Suspense>
-          )}
+          {this.state.page && React.createElement(this.state.page)}
         </div>
       </HistoryContext.Provider>
     );
