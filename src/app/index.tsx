bcb2c2353be73a17a2dadb74983e62133cfc333a
kik4 <kik4.contact@gmail.com>
2019-04-17 02:56:53 +0900

    remove hot condition

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,7 +7,3 @@ import ReactDOM from "react-dom";
 import App from "./app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-if ((module as any).hot) {
-  (module as any).hot.accept();
-}
