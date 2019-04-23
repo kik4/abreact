7a7ba5596e5bdade75c552ed27b9a986c17661e3
kik4 <kik4.contact@gmail.com>
2019-04-24 01:07:30 +0900

    fix console message

--- src/command/clear.ts
+++ src/command/clear.ts
@@ -3,5 +3,10 @@ import path from "path";
 import { CommonParams } from "./type";
 
 export default (commonParams: CommonParams) => {
-  rimraf(path.join(commonParams.userRoot, "dist"), err => console.error(err));
+  rimraf(path.join(commonParams.userRoot, "dist"), err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
 };
