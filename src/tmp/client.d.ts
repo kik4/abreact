f2fa02e8096c9c14580879bff0996dfea5b97285
kik4 <kik4.contact@gmail.com>
2019-04-24 02:50:26 +0900

    add tmp file type declaration

---
+++ src/tmp/client.d.ts
@@ -0,0 +1,9 @@
+import { AbreactPage } from "../common/types";
+type Routes = Route<any, RouteAction>[];
+
+export declare const modules: {
+  [key: string]: () => Promise<AbreactPage | undefined>;
+};
+export declare const routes: Routes;
+export declare const layouts: { [key: string]: string };
+export declare const plugins: { [key: string]: () => any };
